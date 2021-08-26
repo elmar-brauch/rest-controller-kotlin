9e6786bbd93189978a6cffbb52fde292cbc8773b
Elmar Brauch <elmar.brauch@telekom.de>
2021-08-26 08:55:18 +0200

    Annotation removed

--- src/main/kotlin/de/bsi/rest/Item.kt
+++ src/main/kotlin/de/bsi/rest/Item.kt
@@ -2,4 +2,4 @@ package de.bsi.rest
 
 import java.util.*
 
-data class Item @JvmOverloads constructor(val name: String, val id: String, val date: Date = Date())
+data class Item constructor(val name: String, val id: String, val date: Date = Date())
